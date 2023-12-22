3aaee6d7e0d328e7782cdcdee15b9dc63cda214e
Omar Ahmed El-Gazzar <92094226+OmmrGAZAR315@users.noreply.github.com>
2023-12-22 14:55:08 +0200

    call uri pagelets in random order

--- src/main/webapp/bigpipe.js
+++ src/main/webapp/bigpipe.js
@@ -1,9 +1,22 @@
+document.addEventListener("DOMContentLoaded", async function () {
+    let RandomPageletesOrder = getUniqueRandomNumber(1, 3)
+
+
+    await sleep(1200);
+    loadPagelet('pagelet' + RandomPageletesOrder[0], 'pagelet' + RandomPageletesOrder[0]);
+
+    await sleep(1500);
+    loadPagelet('pagelet' + RandomPageletesOrder[1], 'pagelet' + RandomPageletesOrder[1]);
+
+    await sleep(1000);
+    loadPagelet('pagelet' + RandomPageletesOrder[2], 'pagelet' + RandomPageletesOrder[2]);
+});
+
 function loadPagelet(url, elementId) {
     fetch(url)
         .then(response => response.text())
         .then(html =>
             document.getElementById(elementId).innerHTML = html
-            // console.log(html)
         );
 }
 
@@ -11,12 +24,19 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-document.addEventListener("DOMContentLoaded", async function () {
-    await sleep(1500);
-    loadPagelet('pagelet2', 'pagelet2');
-    await sleep(2000);
-    loadPagelet('pagelet1', 'pagelet1');
-    await sleep(1500);
-    loadPagelet('pagelet3', 'pagelet3');
-    // Load other pagelets similarly
-});
+function getUniqueRandomNumber(min, max) {
+    let uniqueNumbers = [];
+    while (uniqueNumbers.length < 3) {
+        let randomNumber = getRandomNumber(min, max);
+        console.log(randomNumber)
+
+        if (!uniqueNumbers.includes(randomNumber))
+            uniqueNumbers.push(randomNumber)
+
+    }
+    return uniqueNumbers
+}
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
